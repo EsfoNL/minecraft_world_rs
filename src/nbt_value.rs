18a6a87554fdac19e47da4a7649f5270886bd978
Esra Fokker <esra.fokker@finalist.nl>
2023-04-30 14:22:21 +0200

    added typing to lists

--- src/nbt_value.rs
+++ src/nbt_value.rs
@@ -26,10 +26,27 @@ pub enum NbtValue {
     Double(f64),
     ByteArray(Vec<i8>),
     String(String),
-    List(Vec<NbtValue>),
+    List(NbtList),
     Compound(HashMap<String, NbtValue>),
     IntArray(Vec<i32>),
     LongArray(Vec<i64>),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum NbtList {
+    ByteList(Vec<i8>),
+    ShortList(Vec<i16>),
+    IntList(Vec<i32>),
+    LongList(Vec<i64>),
+    FloatList(Vec<f32>),
+    DoubleList(Vec<f64>),
+    ByteArrayList(Vec<Vec<i8>>),
+    StringList(Vec<String>),
+    ListList(Vec<NbtList>),
+    CompoundList(Vec<HashMap<String, NbtValue>>),
+    IntArrayList(Vec<Vec<i32>>),
+    LongArrayList(Vec<Vec<i64>>),
+    EmptyList,
 }
 
 impl NbtValue {
@@ -42,7 +59,7 @@ impl NbtValue {
         T: Iterator<Item = &'a u8> + Debug,
     {
         let tag = iter.next().ok_or(Error::Malformed(line!()))?.to_owned();
-        let name = Self::get_tag_name(&mut iter)?;
+        let name = Self::string_from_iter(&mut iter)?;
         Ok((name, Self::deserialize(&mut iter, tag)?))
     }
 
@@ -51,133 +68,202 @@ impl NbtValue {
         T: Iterator<Item = &'a u8> + Debug,
     {
         match tag {
-            TAG_BYTE => Ok(NbtValue::Byte(i8::from_be_bytes([iter
-                .next()
-                .ok_or(Error::Malformed(line!()))?
-                .to_owned()]))),
-            TAG_SHORT => Ok(NbtValue::Short(i16::from_be_bytes([
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-            ]))),
+            TAG_BYTE => Ok(NbtValue::Byte(Self::i8_from_iter(iter)?)),
+            TAG_SHORT => Ok(NbtValue::Short(Self::i16_from_iter(iter)?)),
             TAG_INT => Ok(NbtValue::Int(Self::i32_from_iter(iter)?)),
-            TAG_LONG => Ok(NbtValue::Long(i64::from_be_bytes([
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-            ]))),
-            TAG_FLOAT => Ok(NbtValue::Float(f32::from_be_bytes([
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-            ]))),
-            TAG_DOUBLE => Ok(NbtValue::Double(f64::from_be_bytes([
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-            ]))),
-            TAG_BYTE_ARRAY => {
-                let size = Self::i32_from_iter(iter)? as usize;
-                let mut output = Vec::with_capacity(size);
+            TAG_LONG => Ok(NbtValue::Long(Self::i64_from_iter(iter)?)),
+            TAG_FLOAT => Ok(NbtValue::Float(Self::f32_from_iter(iter)?)),
+            TAG_DOUBLE => Ok(NbtValue::Double(Self::f64_from_iter(iter)?)),
+            TAG_BYTE_ARRAY => Ok(NbtValue::ByteArray(Self::byte_array_from_iter(iter)?)),
+            TAG_STRING => Ok(NbtValue::String(Self::string_from_iter(iter)?)),
+            TAG_LIST => Ok(NbtValue::List(Self::list_from_iter(iter)?)),
+            TAG_COMPOUND => Ok(NbtValue::Compound(Self::compound_from_iter(iter)?)),
+            TAG_INT_ARRAY => Ok(NbtValue::IntArray(Self::int_array_from_iter(iter)?)),
+            TAG_LONG_ARRAY => Ok(NbtValue::LongArray(Self::long_array_from_iter(iter)?)),
+            _ => Err(Error::Malformed(line!())),
+        }
+    }
+
+    fn long_array_from_iter<'a, T>(iter: &mut T) -> Result<Vec<i64>>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        let size = Self::i32_from_iter(iter)? as usize;
+        let mut output = Vec::with_capacity(size);
+        for _ in 0..size {
+            output.push(Self::i64_from_iter(iter)?);
+        }
+        Ok(output)
+    }
+
+    fn int_array_from_iter<'a, T>(iter: &mut T) -> Result<Vec<i32>>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        let size = Self::i32_from_iter(iter)? as usize;
+        let mut output = Vec::with_capacity(size);
+        for _ in 0..size {
+            output.push(Self::i32_from_iter(iter)?)
+        }
+        Ok(output)
+    }
+
+    fn compound_from_iter<'a, T>(iter: &mut T) -> Result<HashMap<String, NbtValue>>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        let mut output = HashMap::new();
+        loop {
+            let tag = iter.next().ok_or(Error::Malformed(line!()))?.to_owned();
+            if tag == TAG_END {
+                break;
+            } else {
+                let key = Self::string_from_iter(iter)?;
+                let value = Self::deserialize(iter, tag)?;
+                output.insert(key, value);
+            }
+        }
+        Ok(output)
+    }
+
+    fn list_from_iter<'a, T>(iter: &mut T) -> Result<NbtList>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        let tag = iter.next().ok_or(Error::Malformed(line!()))?.to_owned();
+        let size = Self::i32_from_iter(iter)? as usize;
+        match tag {
+            TAG_BYTE => {
+                let mut output = Vec::new();
                 for _ in 0..size {
-                    output.push(i8::from_be_bytes([iter
-                        .next()
-                        .ok_or(Error::Malformed(line!()))?
-                        .to_owned()]));
+                    output.push(Self::i8_from_iter(iter)?);
                 }
-                Ok(NbtValue::ByteArray(output))
+                Ok(NbtList::ByteList(output))
+            }
+            TAG_SHORT => {
+                let mut output = Vec::new();
+                for _ in 0..size {
+                    output.push(Self::i16_from_iter(iter)?);
+                }
+                Ok(NbtList::ShortList(output))
+            }
+            TAG_INT => {
+                let mut output = Vec::new();
+                for _ in 0..size {
+                    output.push(Self::i32_from_iter(iter)?);
+                }
+                Ok(NbtList::IntList(output))
+            }
+            TAG_LONG => {
+                let mut output = Vec::new();
+                for _ in 0..size {
+                    output.push(Self::i64_from_iter(iter)?);
+                }
+                Ok(NbtList::LongList(output))
+            }
+            TAG_FLOAT => {
+                let mut output = Vec::new();
+                for _ in 0..size {
+                    output.push(Self::f32_from_iter(iter)?);
+                }
+                Ok(NbtList::FloatList(output))
+            }
+            TAG_DOUBLE => {
+                let mut output = Vec::new();
+                for _ in 0..size {
+                    output.push(Self::f64_from_iter(iter)?);
+                }
+                Ok(NbtList::DoubleList(output))
+            }
+            TAG_BYTE_ARRAY => {
+                let mut output = Vec::new();
+                for _ in 0..size {
+                    output.push(Self::byte_array_from_iter(iter)?);
+                }
+                Ok(NbtList::ByteArrayList(output))
             }
             TAG_STRING => {
-                let size = u16::from_be_bytes([
-                    iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                    iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                ]) as usize;
-                let mut output = Vec::with_capacity(size);
+                let mut output = Vec::new();
                 for _ in 0..size {
-                    output.push(iter.next().ok_or(Error::Malformed(line!()))?.to_owned());
+                    output.push(Self::string_from_iter(iter)?);
                 }
-                Ok(NbtValue::String(
-                    String::from_utf8(output).map_err(|_| Error::Malformed(line!()))?,
-                ))
+                Ok(NbtList::StringList(output))
             }
             TAG_LIST => {
-                let tag = iter.next().ok_or(Error::Malformed(line!()))?.to_owned();
-                let size = Self::i32_from_iter(iter)? as usize;
-                let mut output = Vec::with_capacity(size);
+                let mut output = Vec::new();
                 for _ in 0..size {
-                    output.push(Self::deserialize(iter, tag)?)
+                    output.push(Self::list_from_iter(iter)?);
                 }
-                Ok(NbtValue::List(output))
+                Ok(NbtList::ListList(output))
             }
             TAG_COMPOUND => {
-                let mut output = HashMap::new();
-                loop {
-                    let tag = iter.next().ok_or(Error::Malformed(line!()))?.to_owned();
-                    if tag == TAG_END {
-                        break;
-                    } else {
-                        let key = Self::get_tag_name(iter)?;
-                        let value = Self::deserialize(iter, tag)?;
-                        output.insert(key, value);
-                    }
+                let mut output = Vec::new();
+                for _ in 0..size {
+                    output.push(Self::compound_from_iter(iter)?);
                 }
-                Ok(NbtValue::Compound(output))
+                Ok(NbtList::CompoundList(output))
             }
             TAG_INT_ARRAY => {
-                let size = Self::i32_from_iter(iter)? as usize;
-                let mut output = Vec::with_capacity(size);
+                let mut output = Vec::new();
                 for _ in 0..size {
-                    output.push(Self::i32_from_iter(iter)?)
+                    output.push(Self::int_array_from_iter(iter)?);
                 }
-                Ok(NbtValue::IntArray(output))
+                Ok(NbtList::IntArrayList(output))
             }
             TAG_LONG_ARRAY => {
-                let size = Self::i32_from_iter(iter)? as usize;
-                let mut output = Vec::with_capacity(size);
+                let mut output = Vec::new();
                 for _ in 0..size {
-                    output.push(i64::from_be_bytes([
-                        iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                        iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                        iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                        iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                        iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                        iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                        iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                        iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-                    ]))
+                    output.push(Self::long_array_from_iter(iter)?);
                 }
-                Ok(NbtValue::LongArray(output))
+                Ok(NbtList::LongArrayList(output))
             }
+            TAG_END => Ok(NbtList::EmptyList),
             _ => Err(Error::Malformed(line!())),
         }
     }
 
     /// Call after getting tag type
-    fn get_tag_name<'a, T>(iter: &mut T) -> Result<String>
+    fn string_from_iter<'a, T>(iter: &mut T) -> Result<String>
     where
         T: Iterator<Item = &'a u8> + Debug,
     {
-        let length = u16::from_be_bytes([
-            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
-        ]) as usize;
+        let length = Self::u16_from_iter(iter)? as usize;
         let mut buf = Vec::with_capacity(length);
         for _ in 0..length {
             buf.push(iter.next().unwrap().to_owned());
         }
         let string = String::from_utf8(buf).map_err(|_| Error::Malformed(line!()))?;
         Ok(string)
-        //.map_err(|e| Error::Malformed(line!()))
+    }
+
+    fn u16_from_iter<'a, T>(iter: &mut T) -> Result<u16>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        Ok(u16::from_be_bytes([
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+        ]))
+    }
+
+    fn i8_from_iter<'a, T>(iter: &mut T) -> Result<i8>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        Ok(i8::from_be_bytes([iter
+            .next()
+            .ok_or(Error::Malformed(line!()))?
+            .to_owned()]))
+    }
+
+    fn i16_from_iter<'a, T>(iter: &mut T) -> Result<i16>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        Ok(i16::from_be_bytes([
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+        ]))
     }
 
     fn i32_from_iter<'a, T>(iter: &mut T) -> Result<i32>
@@ -190,5 +276,123 @@ impl NbtValue {
             iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
             iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
         ]))
+    }
+
+    fn i64_from_iter<'a, T>(iter: &mut T) -> Result<i64>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        Ok(i64::from_be_bytes([
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+        ]))
+    }
+
+    fn f64_from_iter<'a, T>(iter: &mut T) -> Result<f64>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        Ok(f64::from_be_bytes([
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+        ]))
+    }
+
+    fn f32_from_iter<'a, T>(iter: &mut T) -> Result<f32>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        Ok(f32::from_be_bytes([
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+            iter.next().ok_or(Error::Malformed(line!()))?.to_owned(),
+        ]))
+    }
+
+    fn byte_array_from_iter<'a, T>(iter: &mut T) -> Result<Vec<i8>>
+    where
+        T: Iterator<Item = &'a u8> + Debug,
+    {
+        let size = Self::i32_from_iter(iter)? as usize;
+        let mut output = Vec::with_capacity(size);
+        for _ in 0..size {
+            output.push(i8::from_be_bytes([iter
+                .next()
+                .ok_or(Error::Malformed(line!()))?
+                .to_owned()]));
+        }
+        Ok(output)
+    }
+
+    fn to_bytes(&self, name: &str) -> Vec<u8> {
+        let mut buffer = Vec::new();
+        match self {
+            NbtValue::Byte(v) => {
+                buffer.push(TAG_BYTE);
+                Self::push_name(&mut buffer, name);
+                buffer.extend_from_slice(&v.to_be_bytes());
+            }
+            NbtValue::Short(v) => {
+                buffer.push(TAG_SHORT);
+                Self::push_name(&mut buffer, name);
+                buffer.extend_from_slice(&v.to_be_bytes());
+            }
+            NbtValue::Int(v) => {
+                buffer.push(TAG_INT);
+                Self::push_name(&mut buffer, name);
+                buffer.extend_from_slice(&v.to_be_bytes());
+            }
+            NbtValue::Long(v) => {
+                buffer.push(TAG_LONG);
+                Self::push_name(&mut buffer, name);
+                buffer.extend_from_slice(&v.to_be_bytes());
+            }
+            NbtValue::Float(v) => {
+                buffer.push(TAG_FLOAT);
+                Self::push_name(&mut buffer, name);
+                buffer.extend_from_slice(&v.to_be_bytes());
+            }
+            NbtValue::Double(v) => {
+                buffer.push(TAG_DOUBLE);
+                Self::push_name(&mut buffer, name);
+                buffer.extend_from_slice(&v.to_be_bytes());
+            }
+            NbtValue::ByteArray(v) => {
+                buffer.push(TAG_BYTE_ARRAY);
+                Self::push_name(&mut buffer, name);
+                buffer.extend_from_slice(&(v.len() as i32).to_be_bytes());
+                buffer.extend(v.iter().map(|f| f.to_be_bytes()[0]));
+            }
+            NbtValue::String(v) => {
+                buffer.push(TAG_BYTE_ARRAY);
+                Self::push_name(&mut buffer, name);
+                buffer.extend_from_slice(&(v.len() as u16).to_be_bytes());
+                buffer.extend_from_slice(v.as_bytes());
+            }
+            NbtValue::List(v) => {
+                buffer.push(TAG_LIST);
+            }
+            NbtValue::Compound(_) => todo!(),
+            NbtValue::IntArray(_) => todo!(),
+            NbtValue::LongArray(_) => todo!(),
+        }
+        buffer
+    }
+    fn push_name(buffer: &mut Vec<u8>, name: &str) {
+        buffer.extend_from_slice(&(name.len() as u32).to_be_bytes());
+        buffer.extend_from_slice(name.as_bytes());
     }
 }
